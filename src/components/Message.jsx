9e1134f5a33b6f58d6844d4b8ac7bf1333225e9c
aswanthkt31 <aswanthkt99@gmail.com>
2025-12-31 17:57:28 +0530

    feat: implement chat message handling and response generation

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,21 +3,20 @@ import userLogo from "../assets/user-logo.png";
 import botLogo from "../assets/chatbot-logo.avif";
 
 function Message(props) {
-
   return (
     <>
       {props.chatMessages.map((chatMessage) => {
-          return(
-              <div key={ chatMessage.id }>
-                  { chatMessage.sender === "user" && chatMessage.message && (
-                    <img src={userLogo} alt="user-lgo" width={20} />
-                  ) }
-                  { chatMessage.message }
-                  { chatMessage.sender === "robot" && chatMessage.message && (
-                    <img src={botLogo} alt="bot" width={30} />
-                  ) }
-              </div>
-          )
+        return (
+          <div key={chatMessage.id}>
+            {chatMessage.sender === "user" && chatMessage.message && (
+              <img src={userLogo} alt="user-lgo" width={20} />
+            )}
+            {chatMessage.message}
+            {chatMessage.sender === "robot" && chatMessage.message !== '' && (
+              <img src={botLogo} alt="bot" width={30} />
+            )}
+          </div>
+        );
       })}
     </>
   );
